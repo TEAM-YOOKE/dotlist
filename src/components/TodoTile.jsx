7ffbcda0384a44d71e3fca97321777a484dc6bc9
mawuse-tech <akpekemawuse1@gmail.com>
2024-11-01 15:06:53 +0000

    first push

--- src/components/TodoTile.jsx
+++ src/components/TodoTile.jsx
@@ -1,8 +1,18 @@
+import { div } from 'framer-motion/client'
 import React from 'react'
+import { HiOutlineTrash } from "react-icons/hi";
 
-const TodoTile = () => {
+const TodoTile = ({todo}) => {
   return (
-    <div>TodoTile</div>
+    <div>
+      <div className='h-[72px] bg-[#262626] flex gap-x-[12px] px-[16px] py-[16px]'>
+        < input type="checkbox" className='rounded-full'/>
+        <h3>
+        Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
+          </h3>
+        <HiOutlineTrash />
+      </div>
+    </div>
   )
 }
 
