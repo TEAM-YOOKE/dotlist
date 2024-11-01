786289aeaeb66eacf0a157aabc7f615b9989f287
Governor Narh <ron468896@gmail.com>
2024-11-01 16:40:34 +0000

    Login page modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -6,8 +6,25 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./pages/login";
 import UserRoute from "./routes/UserRoute";
 import Todos from "./pages/todos";
-
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
 function App() {
+  const [user, setUser] = useState({})
+  const [loading, setLoading] = useState(false)
+const dispatch = useDispatch
+  useEffect(() => {
+    setLoading(true);
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        console.log("Logged In user", user);
+        const idTokenResult = await user.getIdTokenResult();
+      }
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, [dispatch]);
+
   const [count, setCount] = useState(0);
 
   const router = createBrowserRouter([
