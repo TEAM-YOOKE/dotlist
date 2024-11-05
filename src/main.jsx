856282ad79a6cb50e853b237ed044330a4822dc7
Governor Narh <ron468896@gmail.com>
2024-11-05 16:38:43 +0000

    PWA start

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+serviceWorkerRegistration.register();
 
 import { Provider } from "react-redux";
 import store from "./reducers";
