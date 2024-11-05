9e021b1ed3fc37afe00d28923229dd28119aa9ab
Abugre Abayeta <abayetaabugre@gmail.com>
2024-11-05 14:08:03 +0000

    update

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -24,7 +24,7 @@ function AddTodo({ addTodo, loading }) {
   return (
     <form
       onSubmit={handleCreateTask}
-      className="flex items-center rounded-lg w-full h-[54] -mt-6 mb-10 gap-2 justify-start"
+      className="flex flex-col md:flex-row items-center rounded-lg w-full -mt-6 mb-10 gap-2 md:gap-4 justify-start"
     >
       <input
         type="text"
@@ -32,19 +32,19 @@ function AddTodo({ addTodo, loading }) {
         placeholder="Adicione uma nova tarefa"
         value={task} // Bind input to task state
         onChange={(e) => setTask(e.target.value)} // Update task state on input change
-        className="flex-1 p-2 text-white bg-[#262626] placeholder-[#808080] rounded-md outline-none focus:ring-2 focus:ring-blue-500 h-[54px]"
+        className="w-full md:flex-1 p-2 text-white bg-[#262626] placeholder-[#808080] rounded-md outline-none focus:ring-2 focus:ring-blue-500 h-[54px]"
       />
       <input
         type="datetime-local" // Input type for date and time
         name="deadline"
         value={deadline} // Bind input to deadline state
         onChange={(e) => setDeadline(e.target.value)} // Update deadline state on input change
-        className="p-2 text-white bg-[#262626] placeholder-[#808080] rounded-md outline-none focus:ring-2 focus:ring-blue-500 h-[54px]"
+        className="w-full md:flex-1 p-2 text-white bg-[#262626] placeholder-[#808080] rounded-md outline-none focus:ring-2 focus:ring-blue-500 h-[54px]"
       />
       <button
         type="submit"
         disabled={loading}
-        className="flex bg-[#1e6f9f] text-white p-4 rounded-md transition duration-300 gap-x-2 text-sm font-bold items-center"
+        className="w-full md:w-auto flex bg-[#1e6f9f] text-white p-4 rounded-md transition duration-300 gap-x-2 text-sm font-bold items-center justify-center"
       >
         {loading ? "loading" : "Criar"}
         <IoIosAddCircleOutline className="h-[16px] w-[16px]" />
