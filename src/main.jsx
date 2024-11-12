48278bf17ef63dccd8e6336d45eacab26fc84e91
Governor Narh <ron468896@gmail.com>
2024-11-12 14:57:08 +0000

    push notifictaion v1

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { onMessage } from "firebase/messaging";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -10,35 +9,9 @@ import App from './App.jsx'
 import { Provider } from "react-redux";
 import store from "./reducers";
 
-import { messaging } from './firebase.js';
 
-// Request permission to show notifications
-const requestPermission = async () => {
-  try {
-    await Notification.requestPermission();
-    console.log("Notification permission granted.");
-  } catch (error) {
-    console.error("Unable to get permission to notify.", error);
-  }
-};
 
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register(`/firebase-messaging-sw.js`)
-    .then((registration) => {
-      console.log("Service Worker registered", registration);
-      requestPermission(); // Request notification permission here
-    })
-    .catch((err) => console.error("Service Worker registration failed", err));
-}
 
-// Handle foreground messages
-onMessage(messaging, (payload) => {
-  console.log("Message received in foreground: ", payload);
-  // Display notification
-  const { title, body } = payload.notification;
-  new Notification(title, { body });
-});
 
 // navigator.serviceWorker
 //   .register('/firebase-messaging-sw.js')
